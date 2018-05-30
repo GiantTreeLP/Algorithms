6b683674a600f38a4c79763ac4d4db5d80b3ed03
GiantTree <marv.1997@live.de>
2018-05-30 19:10:34 +0200

    add boyer-moore and rabin karp search

--- string_search.py
+++ string_search.py
@@ -1,4 +1,7 @@
-def brute_force_search(needle, haystack):
+from typing import List
+
+
+def brute_force_search(needle: str, haystack: str) -> int:
     m = len(needle)
     haystack += needle
     i = j = 0
@@ -11,8 +14,8 @@ def brute_force_search(needle, haystack):
     return i - m
 
 
-def kmp_search(needle, haystack):
-    def initnext(s):
+def kmp_search(needle: str, haystack: str) -> int:
+    def initnext(s: str) -> List[int]:
         n = [0] * (len(s) + 1)
         n[0] = -1
         i = 0
@@ -38,7 +41,50 @@ def kmp_search(needle, haystack):
     return i - m
 
 
+def boyer_moore_search(needle: str, haystack: str) -> int:
+    def initskip(s: str) -> List[int]:
+        skip = [0] * 256
+        m = len(s)
+        for j in range(256):
+            skip[j] = m
+        for j in range(len(s)):
+            skip[ord(s[j])] = m - j - 1
+        return skip
+
+    m = len(needle)
+    skip = initskip(needle)
+    i = j = m - 1
+    while j > 0:
+        while haystack[i] != needle[j]:
+            t = skip[ord(haystack[i])]
+            i += max(m - j, t)
+            j = m - 1
+        i -= 1
+        j -= 1
+    return i
+
+
+def rabin_karp_search(needle: str, haystack: str) -> int:
+    b = 256
+    q = 8388593
+    m = len(needle)
+    B = b ** (m - 1)
+    hp = ha = 0
+    haystack += needle
+    for i in range(m):
+        hp = (hp * b + ord(needle[i])) % q
+        ha = (ha * b + ord(haystack[i])) % q
+    i = 0
+    while hp != ha:
+        ha = (ha - B * ord(haystack[i])) % q
+        ha = (ha * b + ord(haystack[i + m])) % q
+        i += 1
+    return i
+
+
 if __name__ == '__main__':
     print(brute_force_search("fox", "The lazy brown fox jumps over the lazy dog"))
     print(kmp_search("fox", "The lazy brown fox jumps over the lazy dog"))
+    print(boyer_moore_search("fox", "The lazy brown fox jumps over the lazy dog"))
+    print(rabin_karp_search("fox", "The lazy brown fox jumps over the lazy dog"))
     print("The lazy brown fox jumps over the lazy dog".find("fox"))
