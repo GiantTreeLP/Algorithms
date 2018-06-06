b0f22885deeaa4de308dd66b92866b8356232e02
GiantTree <marv.1997@live.de>
2018-06-06 14:23:58 +0200

    Add simple heapsort algorithm using the builtin heap functions

--- sorting.py
+++ sorting.py
@@ -1,3 +1,6 @@
+from heapq import heappush, heappop
+
+
 def selection_sort(sequence):
     swaps = 0
     comparisons = 0
@@ -62,8 +65,19 @@ def quick_sort(x):
         return quick_sort(less) + [pivot] + quick_sort(greater)
 
 
+def heap_sort(sequence):
+    heap = []
+    for v in sequence:
+        heappush(heap, v)
+    result = []
+    while heap:
+        result.append(heappop(heap))
+    return result
+
+
 if __name__ == '__main__':
     print(selection_sort([2, 4, 6, 4, 2, 7, 3, 1, 8, 6, 8, 4]))
     print(insertion_sort([2, 4, 6, 4, 2, 7, 3, 1, 8, 6, 8, 4]))
     print(merge_sort([2, 4, 6, 4, 2, 7, 3, 1, 8, 6, 8, 4]))
     print(quick_sort([2, 4, 6, 4, 2, 7, 3, 1, 8, 6, 8, 4]))
+    print(heap_sort([2, 4, 6, 4, 2, 7, 3, 1, 8, 6, 8, 4]))
