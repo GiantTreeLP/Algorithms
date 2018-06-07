a17d40b33f688575aac93724aa0788c7b5501858
Marvin <marv.1997@live.de>
2018-06-07 18:41:25 +0200

    Compress and decompress from and to files, add some type annotations

--- encode-encrypt/huffmann.py
+++ encode-encrypt/huffmann.py
@@ -1,9 +1,11 @@
+import json
+import os
 from heapq import heapify, heappop, heappush
 from pathlib import Path
-from typing import Dict, List
+from typing import Dict, List, Tuple, Iterable
 
 
-def frequency(input_bytes) -> Dict[object, int]:
+def frequency(input_bytes: Iterable) -> Dict[object, int]:
     result = {}
     for c in input_bytes:
         if c in result:
@@ -13,13 +15,13 @@ def frequency(input_bytes) -> Dict[object, int]:
     return result
 
 
-def initheap(frequencies) -> List[object]:
+def initheap(frequencies: Dict[chr, int]) -> List[Tuple[int, List[Tuple[chr, Tuple[int, int]]]]]:
     heap = [(f, [(s, (0, 0))]) for s, f in frequencies.items()]
     heapify(heap)
     return heap
 
 
-def encode(input_bytes, table):
+def encode(input_bytes: Iterable[int], table: Dict[int, Tuple[int, int]]) -> str:
     result: str = ""
     for s in input_bytes:
         b, v = table[s]
@@ -27,11 +29,11 @@ def encode(input_bytes, table):
     return result
 
 
-def format_binary(b, v):
+def format_binary(b: int, v: int) -> str:
     return bin(v)[2:].rjust(b, '0')
 
 
-def encoding_table(input_bytes):
+def encoding_table(input_bytes: Iterable[int]) -> Dict[int, Tuple[int, int]]:
     frequencies = frequency(input_bytes)
     heap = initheap(frequencies)
     while len(heap) > 1:
@@ -44,16 +46,53 @@ def encoding_table(input_bytes):
     return dict(heappop(heap)[1])
 
 
+def compress(encoded: str) -> Tuple[bytes, int]:
+    padding = (8 - (len(encoded) % 8)) % 8
+    encoded += '0' * padding
+    array = bytes([int(encoded[i:i + 8], 2) for i in range(0, len(encoded), 8)])
+    return array, padding
+
+
+def decompress(compressed: bytes) -> str:
+    return ''.join([format_binary(8, b) for b in compressed])
+
+
+def write_to_file(file_name: str, compressed: bytes, table: Dict[int, Tuple[int, int]], padding: int) -> None:
+    with open(file_name, mode="wb") as f:
+        obj = {'p': padding,
+               't': table}
+        f.write(json.dumps(obj, ensure_ascii=False, separators=(',', ':')).encode('UTF-8'))
+        f.write(bytes([0]))
+        f.write(compressed)
+        f.close()
+
+
+def compress_to_file(file_name: str, input_bytes: Iterable[int]) -> None:
+    table = encoding_table(input_bytes)
+    compressed, padding = compress(encode(input_bytes, table))
+    write_to_file(file_name, compressed, table, padding)
+
+
+def read_from_file(file_name: str) -> Iterable[int]:
+    binary = Path(file_name).read_bytes()
+    null_index = binary.find(0)
+    obj = json.loads(binary[0:null_index], encoding='UTF-8')
+    compressed = binary[null_index + 1:]
+    decompressed = decompress(compressed)[0:len(compressed) * 8 - obj['p']]
+    return decode(decompressed, obj['t'])
+
+
 def test_encode() -> None:
     input_bytes = Path("../resources/loremipsum.txt").read_text()
-    table = encoding_table(input_bytes)
-    encoded = encode(input_bytes, table)
-    print(encoded)
-    decoded = decode(encoded, table)
-    print(decoded)
+    compress_to_file("compressed.huff", input_bytes)
+
+    # iterable = read_from_file("compressed.huff")
+    print("Size original:", len(input_bytes))
+    print("Size compressed:", os.stat("compressed.huff").st_size)
+    print("Net savings: {:%}".format(1 - os.stat("compressed.huff").st_size / len(input_bytes)))
 
 
-def decode(encoded, table):
+def decode(encoded: str, table: Dict[chr, Tuple[int, int]]) -> Iterable:
     decoding_table = dict()
     for s, (b, v) in table.items():
         decoding_table[format_binary(b, v)] = (s, b)
